7d05fbf6717ef267432acb8a638d92fd9488873d
AmirHossein Jahankheir Amlashi <amir.cph4@gmail.com>
2024-09-01 09:19:13 +0330

    tamrin 2

--- 02-plaindrom/plaidrom.js
+++ 02-plaindrom/plaidrom.js
@@ -1,1 +1,11 @@
 // write your func here
+// function that reverse input string
+function reverseString1(str) {
+    return str.split('').reverse().join('');
+}
+// function that checks if string is palindrome
+function isPalindrome(str) {
+    return str === reverseString1(str);
+}
+
+console.log(isPalindrome("racecar"));
