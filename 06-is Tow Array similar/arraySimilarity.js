486c6376038a6c31775cf7bce9ae8e4554e4aebe
AmirHossein Jahankheir Amlashi <amir.cph4@gmail.com>
2024-09-01 09:31:05 +0330

    tamrin 6

--- 06-is Tow Array similar/arraySimilarity.js
+++ 06-is Tow Array similar/arraySimilarity.js
@@ -1,1 +1,11 @@
 // write your func here
+function compare(a, b) {
+    for (let i = 0; i < a.length; i++) {
+        if (a[i] !== b[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(compare([1, 2, 3], [1, 2, 3]));
