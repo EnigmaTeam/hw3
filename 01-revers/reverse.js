9a0cb4bdfb2a7d20e1943b669beffffb7f711784
AmirHossein Jahankheir Amlashi <amir.cph4@gmail.com>
2024-09-01 09:08:15 +0330

    tamrin 1

--- 01-revers/reverse.js
+++ 01-revers/reverse.js
@@ -1,1 +1,16 @@
 // write your func here
+// function that reverse input string
+function reverseString1(str) {
+  return str.split('').reverse().join('');
+}
+//with for loop
+function reverseString2(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
+
+console.log(reverseString1("hello i am amirhossein"));
+console.log(reverseString2("hello i am amirhossein"));
