abda45f6e1914e63b618509c2c7d000f646c3eb3
AmirHossein Jahankheir Amlashi <amir.cph4@gmail.com>
2024-09-01 09:27:29 +0330

    tamrin 5

--- 05-findMax/findMax.js
+++ 05-findMax/findMax.js
@@ -1,1 +1,13 @@
 // write your func here
+// functiom that find max value
+function findMax(arr) {
+    let max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+console.log(findMax([5, 6, 12, 8, 9]));
